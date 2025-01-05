6cd5c8ff63a6bca3b8e41649344016c17f3e860e
MilanaMur <milana.murashkina@yandex.ru>
2025-01-05 18:34:23 +0300

    Удалён лишний код.

--- src/components/burger-constructor-element/burger-constructor-element.tsx
+++ src/components/burger-constructor-element/burger-constructor-element.tsx
@@ -2,29 +2,12 @@ import { FC, memo } from 'react';
 import { BurgerConstructorElementUI } from '@ui';
 import { BurgerConstructorElementProps } from './type';
 
-import { useSelector, useDispatch } from '../../services/store';
-import {
-  constructorSelector,
-  deleteItem,
-  swapIngredient,
-  updateAll
-} from '../../slices/constructor';
-import { TConstructorIngredient } from '@utils-types';
+import { useDispatch } from '../../services/store';
+import { deleteItem, swapIngredient } from '../../slices/constructor';
 
 export const BurgerConstructorElement: FC<BurgerConstructorElementProps> = memo(
   ({ ingredient, index, totalItems }) => {
     const dispatch = useDispatch();
-    const constructorItems = useSelector(constructorSelector.selectItems);
-
-    const swapElements = (
-      state: TConstructorIngredient[],
-      index: number,
-      step: number
-    ) => {
-      const copy = [...state];
-      copy[index] = copy.splice(index + step, 1, copy[index])[0];
-      return copy;
-    };
 
     const handleMoveDown = () => {
       dispatch(swapIngredient({ index: index, step: 1 }));
